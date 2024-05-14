69ef73fc3bc6808c671a63183c69f6b0570e9da2
Wheaxy Sueido <joshgunvk@code.edu.az>
2024-05-14 20:11:42 +0400

    Task Completed

---
+++ Task1 SadeMurekkeb/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿int nomre = 5;
+
+if (nomre == 0 || nomre == 1) ;
+else
+{
+    for (int i = 2; i <= nomre / 2; i++)
+    {
+        if (nomre % i == 0)
+        {
+            Console.WriteLine($"{nomre} - Murekkeb ededdir");
+            return;
+        }
+
+    }
+    Console.WriteLine($"{nomre} - Sade ededdir");
+}
---
+++ Task2Length/Task2Length/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task2Length
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 50505050;
+            int counter = 0;
+            while (number > 0)
+            {
+                number /= 10;
+                counter++;
+            }
+            Console.WriteLine($"Daxil etdiyiniz reqem {counter} mertebelidir ");
+
+            Console.ReadLine();
+        }
+    }
+}
